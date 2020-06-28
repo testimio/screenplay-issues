3b0f0f4f0352c233c373703431ecaa8eb40f2160
Bnaya Peretz <bnaya@testim.io>
2020-06-28 11:00:59 +0300

    Update screenplay version

--- integration-examples/jest/jest.config.js
+++ integration-examples/jest/jest.config.js
@@ -6,8 +6,7 @@ const runId = Date.now().toString();
 module.exports = {
     ...puppeteerPreset,
     reporters: [
-        'default',
-        ['@testim/screenplay/src/jest/reporter/RunConclusion', { runId }],
+        ['@testim/screenplay/src/jest/reporter/default', { runId }],
     ],
     setupFilesAfterEnv: [...puppeteerPreset.setupFilesAfterEnv, '@testim/screenplay/src/jest/forJestSetupFilesAfterEnv'],
     globals: {
